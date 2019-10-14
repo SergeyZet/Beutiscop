e03b294bc42ae02a7f23ef9c0afc99a2944c1250
SergeyZet <nikjoynik@gmail.com>
2019-10-14 15:40:47 +0600

    add header and hero section

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+	output: {
+		filename: "[name].js"
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					query: {
+						presets: [
+							["@babel/preset-env", { modules: false }]
+						]
+					}
+				}
+			}
+		]
+	},
+
+	resolve: {
+		alias: {
+			"%modules%": path.resolve(__dirname, "src/blocks/modules"),
+			"%components%": path.resolve(__dirname, "src/blocks/components")
+		}
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendor",
+					chunks: "all",
+					minChunks: 1
+				}
+			}
+		}
+	},
+};
